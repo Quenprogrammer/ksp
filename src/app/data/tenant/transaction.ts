4b50e667719fdbe25c4178470693057a18dc78a3
QuenProgrammer <adamsadam3667@gmail.com>
2026-01-22 15:16:57 +0100

    last usman update

--- src/app/data/tenant/transaction.ts
+++
@@ -1,7 +0,0 @@
-export const table=[
-  {name:'Date', width:'150px'},
-  {name:'Payee' , width:'150px'},
-  {name:'Amount', width:'120px'},
-  {name:'Payment Method', width:'120px'},
-  {name:'Actions', width:'180px'},
-]
